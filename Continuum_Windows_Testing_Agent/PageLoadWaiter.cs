f11fdd468eae6bbc02cb13bac8718528431aa244
Jason Orcutt <zerodiv@zerodivide.net>
2010-10-28 10:50:12 -0700

    beginning conversion to use the selenese support from the openqa project itself

---
+++ Continuum_Windows_Testing_Agent/PageLoadWaiter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace Continuum_Windows_Testing_Agent
+{
+    public class PageLoadWaiter : Waiter
+    {
+        private IWebDriver driver;
+        private int timeToWaitAfterPageLoad;
+        private DateTime started = DateTime.Now;
+
+        public PageLoadWaiter(IWebDriver driver, int timeToWaitAfterPageLoad)
+            : base()
+        {
+            this.driver = driver;
+            this.timeToWaitAfterPageLoad = timeToWaitAfterPageLoad;
+        }
+
+        public override bool Until()
+        {
+            try
+            {
+                object result = ((IJavaScriptExecutor)driver).ExecuteScript("return document['readyState'] ? 'complete' == document.readyState : true");
+
+                DateTime now = DateTime.Now;
+                if (result != null && result is bool && (bool)result)
+                {
+                    if (now.Subtract(started).TotalMilliseconds > timeToWaitAfterPageLoad)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    started = now;
+                }
+            }
+            catch (Exception)
+            {
+                // Possible page reload. Fine
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Continuum_Windows_Testing_Agent/Selenese/SeleneseRemoveSelection.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace Continuum_Windows_Testing_Agent
+{
+
+    class SeleneseRemoveSelection : Selenese_Command
+    {
+
+        public SeleneseRemoveSelection(Selenium_Test_Log log, IWebDriver webDriver) : base(log,webDriver) {
+        }
+
+        public override Boolean run(Selenium_Test_Trinome testCommand)
+        {
+
+            this.log.startTimer();
+
+            // label=bar
+            String label = "";
+            if (testCommand.getValue().StartsWith("label="))
+            {
+                label = testCommand.getValue();
+                label = label.Replace("label=", "");
+            }
+            else
+            {
+                this.log.logFailure(testCommand, "Value is expected to be in the form of label=");
+                return false;
+            }
+
+            // First attempte is via constructed xpath.
+
+            IWebElement option = null;
+            Boolean optionFound = false;
+
+            try
+            {
+                String xByNamePath = "//select[@name='" + testCommand.getTarget() + "']/option[normalize-space(.)='" + label + "']";
+                option = this.webDriver.FindElement(this.locator.convert(xByNamePath));
+                optionFound = true;
+            }
+            catch
+            {
+                optionFound = false;
+            }
+
+            // Try by id if the option was not found. 
+            if (optionFound == false)
+            {
+                try
+                {
+                    String xByIdPath = "//select[@id='" + testCommand.getTarget() + "']/option[normalize-space(.)='" + label + "']";
+                    option = this.webDriver.FindElement(this.locator.convert(xByIdPath));
+                    optionFound = true;
+                }
+                catch
+                {
+                    optionFound = false;
+                }
+            }
+
+            if (optionFound == false)
+            {
+                this.log.logFailure(testCommand, "Failed to find option");
+                return false;
+            }
+
+            try
+            {
+                if (option.Selected == true)
+                {
+                    option.Select();
+                }
+                this.log.logSuccess(testCommand, "");
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                this.log.logFailure(testCommand, e.Message);
+            }
+
+
+            return false;
+
+            /*
+            try
+            {
+                
+
+                IWebElement element = this.webDriver.FindElement(this.convertSelenseLocatorString(testCommand.target));
+                
+                System.Collections.ObjectModel.ReadOnlyCollection<IWebElement> options = element.FindElements(By.TagName("option"));
+
+                foreach (IWebElement option in options)
+                {
+                    if (option.Text == label)
+                    {
+                        option.Select();
+                        this.log.logSuccess(testCommand, "");
+                        return true;
+                    }
+                }
+                this.log.logFailure(testCommand, "Did not find the target label=" + label);
+                return true;
+            }
+            catch (Exception e)
+            {
+                this.log.logFailure(testCommand, e.Message);
+            }
+            */
+
+        }
+    }
+
+}
